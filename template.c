d21ebc9f4e2ba20daa7e392a343ca0a4281300cd
nam-bolbo-na <hasib.jhd@gmail.com>
2017-08-03 23:52:23 +0600

    temp
    temp

---
+++ template.c
@@ -0,0 +1,44 @@
+
+/*-----------------With the name of ALLAH--------------*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define dl double
+#define sf scanf
+#define pf printf
+
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,1,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+
+#define loop(i,ini,n) for(ll i=ini;i<=n;i++)
+#define rloop(i,ini,n) for(ll i=ini;i>=n;i--)
+#define rev_vector(a,n) reverse(begin(a),begin(a)+n)
+
+#define MAX 100
+#define mp  make_pair
+#define pr pair<ll,ll>
+#define pb push_back
+
+ll knitX[]={+2,+2,-2,-2,+1,+1,-1,-1};
+ll knitY[]={+1,-1,+1,-1,+2,-2,+2,-2};
+
+int main()
+{
+    //freopen("inp.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    ll T;
+    sf("%lld",&T);
+    loop(i,1,T)
+    {
+	
+    }
+    return 0;
+}
+https://github.com/DollarAkshay/C-and-CPP-Programs/blob/master/Competitive%20Coding/SPOJ_Prime_Generator.cpp#L22
+
+sl:  120 385 245 981
+pin: 2266 0743 8615
